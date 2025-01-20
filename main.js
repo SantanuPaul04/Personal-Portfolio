0be05b35d7f52c32a6d0e33bb6be37604c07cf3a
Santanu Paul <142681091+SantanuPaul04@users.noreply.github.com>
2025-01-20 22:11:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ readmorebtn.addEventListener('click', (e)=>{
         
         readmorebtn.innerText = 'Read Less';
          
-        document.querySelectorAll('.about-text')[0].style.height = "160vh";
+        document.querySelectorAll('.about-text')[0].style.height = "100%";
         document.querySelectorAll('.container')[0].style.padding = "0vh 5vw";
     }
     
